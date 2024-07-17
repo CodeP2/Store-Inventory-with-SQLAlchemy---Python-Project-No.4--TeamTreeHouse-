df93edc548dbf494e4efab7da81d2a8457dc1b63
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2024-07-17 17:25:13 +0200

    completed menus.py file

--- menus.py
+++ menus.py
@@ -1,14 +1,31 @@
 def main_menu():
-    pass
+    print("""Inventory Program Main Menu 
+          \rv) Search product
+          \ra) Add a new product
+          \rb) backup your database entries
+        """)
 
 
 def menu_v():
-    pass
+    print("""Search Product Menu
+          \r1) Search by id
+          \r2) Search by date
+          \r3) Search by name
+          \r4) Exit Program
+          """)
 
 
 def menu_a():
-    pass
+    print("""Add/Edit/Delete Product Menu
+          \r1) Add a new Product
+          \r2) Edit a product
+          \r3) Delete a product
+          \r4) Exit Menu
+          """)
 
 
 def menu_b():
-    pass
+    print("""Back-up Database Menu
+          \r1) To back-up your database
+          \r2) Exit Menu
+          """)
