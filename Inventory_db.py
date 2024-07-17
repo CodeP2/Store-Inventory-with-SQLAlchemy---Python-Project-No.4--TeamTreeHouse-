655f6de1f048f21ce59084b5f551fcc9a7bb8204
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2024-07-17 14:11:46 +0200

    creating .db and updating README

---
+++ Inventory_db.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine, Column, String, Integer, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.types import Numeric
+
+
+engine = create_engine("sqlite:///Inventory.db", echo=False)
+Session = sessionmaker(bind=engine)
+session = Session
+Base = declarative_base()
+
+
+class Product(Base):
+    __tablename__ = "Produts"
+
+    product_id = Column(Integer, primary_key=True, autoincrement=True)
+    product_name = Column(String)
+    product_quantity = Column(Integer)
+    product_price = Column(Numeric(10, 2))
+    product_updated = Column(Date)
+
+    def __repr__(self):
+        return f"<id: {self.product_id}, name: {self.product_name}, quantity: {self.product_quantity}\
+            price: {self.product_price}, updated: {self.product_updated}>"
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
