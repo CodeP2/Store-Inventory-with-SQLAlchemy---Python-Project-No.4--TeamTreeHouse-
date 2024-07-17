a6b3bd8f3ae2fb58ad53dcca4843af29ef5e126a
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2024-07-17 16:39:37 +0200

    corrected typo

--- Inventory_db.py
+++ Inventory_db.py
@@ -11,7 +11,7 @@ Base = declarative_base()
 
 
 class Product(Base):
-    __tablename__ = "Produts"
+    __tablename__ = "Products"
 
     product_id = Column(Integer, primary_key=True, autoincrement=True)
     product_name = Column(String)
